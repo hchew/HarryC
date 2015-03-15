8633a4d2da4be83daa21e8a2dc15cc81f850ddba
hchew1@gmail.com <hchew1@gmail.com>
2015-03-15 17:49:17 +0000

    whatthefuck

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,7 +19,8 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("hellossss");
+            MessageBox.Show("help me");
+
         }
     }
 }
